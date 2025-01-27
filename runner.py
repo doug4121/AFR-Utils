df668fe7b7171e96af3ed2d8712a4514eb66f1df
Douglas Chang <doug4121@gmail.com>
2025-01-26 21:15:08 -0800

    copied repro

---
+++ runner.py
@@ -0,0 +1,11 @@
+import sys
+
+sys.path.insert(1, '/AFR-Core')
+
+from fawkes.protection import Fawkes
+import os
+
+mode = sys.argv[2]
+image_paths = {sys.argv[1]}
+my_fawkes = Fawkes("extractor_2", '0', 1, mode)
+my_fawkes.run_protection(image_paths, debug=True)
