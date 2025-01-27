9c969699b0cdc8d9962a56c48639bb90774dfa13
Douglas Chang <doug4121@gmail.com>
2025-01-26 23:44:36 -0800

    fix arg to argv

--- runner.py
+++ runner.py
@@ -6,7 +6,7 @@ from fawkes.protection import Fawkes
 import os
 
 mode = sys.argv[2]
-extension = sys.arg[3]
+extension = sys.argv[3]
 image_paths = {sys.argv[1]}
 my_fawkes = Fawkes("extractor_2", '0', 1, mode)
 my_fawkes.run_protection(image_paths, debug=True, format=extension)
