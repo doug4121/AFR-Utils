c178366b5ac55ce21da280f56eb34ffe0dc748d0
Douglas Chang <doug4121@gmail.com>
2025-01-26 22:43:24 -0800

    updated runner to take extension

--- runner.py
+++ runner.py
@@ -6,6 +6,7 @@ from fawkes.protection import Fawkes
 import os
 
 mode = sys.argv[2]
+extension = sys.arg[3]
 image_paths = {sys.argv[1]}
 my_fawkes = Fawkes("extractor_2", '0', 1, mode)
-my_fawkes.run_protection(image_paths, debug=True)
+my_fawkes.run_protection(image_paths, debug=True, format=extension)
