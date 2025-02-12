c89c91f2ed754de7d772acce5da8b766e780a3d9
Douglas Chang <doug4121@gmail.com>
2025-02-12 06:02:25 -0800

    Update bototest.py
    asumed role isnt working

--- bototest.py
+++ bototest.py
@@ -2,16 +2,6 @@ from boto3.s3.transfer import S3Transfer
 import boto3
 from datetime import datetime
 
-sts_client = boto3.client('sts')
-assumed_role_object=sts_client.assume_role(
-    RoleArn="arn:aws:iam::195039344825:role/AFR",
-    RoleSessionName="AssumeRoleSession1"
-)
-credentials=assumed_role_object['Credentials']
-
-sessionToken = credentials['SessionToken']
-accessKey = credentials['AccessKeyId']
-secretKey = credentials['SecretAccessKey']
 region = 'us-west-2'
 tableName = 'imageprocessing'
 imageId = '3d468884-ab83-4468-8abe-1c221dddc554'
@@ -25,13 +15,10 @@ time = isoformat
 
 dynamodb = boto3.resource(
     'dynamodb', 
-    aws_access_key_id = accessKey, 
-    aws_secret_access_key = secretKey,
-    region_name = region,
-    aws_session_token = sessionToken)
+    region_name = region)
 table = dynamodb.Table(tableName)
 
-currentDateTime = get_date_time()
+currentDateTime = time
 table.update_item(
     Key = {'imageId': imageId},
     ConditionExpression = 'attribute_exists(imageId)',
